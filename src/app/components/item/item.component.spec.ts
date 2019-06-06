648328f3522730c8650d1e0f8bddf1bbf81118c8
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-06 13:08:04 -0300

    add item.component.spec.ts

--- src/app/components/item/item.component.spec.ts
+++ src/app/components/item/item.component.spec.ts
@@ -1,18 +1,18 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ItemComponent } from './item.component';
+import { ItemComponent } from "./item.component";
+import { TimeAgoPipe } from "../time-ago/time-ago.pipe";
 
-describe('ItemComponent', () => {
+describe("ItemComponent", () => {
   let component: ItemComponent;
   let fixture: ComponentFixture<ItemComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+      declarations: [ItemComponent, TimeAgoPipe],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,7 +21,7 @@ describe('ItemComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
