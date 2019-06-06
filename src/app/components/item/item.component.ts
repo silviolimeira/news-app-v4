1c0a020a071e3bd5d2033ae86e3384ff6c1f10be
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-06 10:27:26 -0300

    Listing 5-10 add Item component

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+import { Item } from "../../models/item";
+
+@Component({
+  selector: "app-item",
+  templateUrl: "./item.component.html",
+  styleUrls: ["./item.component.scss"]
+})
+export class ItemComponent implements OnInit {
+  @Input() item: Item;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
