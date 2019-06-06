d4f6839b6615826f6ec03afa357634266915f0e4
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-06 14:35:23 -0300

    Listing 5-14 add Template of items component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ItemComponent } from "./item/item.component";
 import { TimeAgoPipe } from "./time-ago/time-ago.pipe";
+import { ItemsComponent } from "./items/items.component";
 
 @NgModule({
-  declarations: [ItemComponent, TimeAgoPipe],
+  declarations: [ItemComponent, TimeAgoPipe, ItemsComponent],
   imports: [CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  exports: [ItemComponent]
+  exports: [ItemComponent, ItemsComponent]
 })
 export class ComponentsModule {}
