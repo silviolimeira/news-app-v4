38056b32e529c51b22bdbe2e986f3bd3d0d59dc0
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-06 13:07:21 -0300

    Listing 5-12 timeAgo pipe

--- src/app/components/time-ago/time-ago.pipe.ts
+++ src/app/components/time-ago/time-ago.pipe.ts
@@ -1,14 +1,10 @@
 import { Pipe, PipeTransform } from "@angular/core";
-
 import * as moment from "moment";
 
 @Pipe({
   name: "timeAgo"
 })
 export class TimeAgoPipe implements PipeTransform {
-  // transform(value: any, args?: any): any {
-  //   return null;
-  // }
   transform(time: number): string {
     return moment.duration(moment().diff(moment(time * 1000))).humanize();
   }
