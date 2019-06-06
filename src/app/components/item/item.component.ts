fb32e0d3e8b426b3cd02c9c9bd5daf4c2d7b26c8
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-06 13:06:38 -0300

    Listing 5-10 and 5-11 Item component and template

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from "@angular/core";
-
-import { Item } from "../../models/item";
+import { Component, Input } from "@angular/core";
+import { Item } from "src/app/models/item";
 
 @Component({
   selector: "app-item",
   templateUrl: "./item.component.html",
   styleUrls: ["./item.component.scss"]
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent {
+  // @Input() item: Item = {
+  //   id: 1,
+  //   title: "fake title",
+  //   url: "fake url",
+  //   by: "fake by",
+  //   time: 0,
+  //   score: 0,
+  //   text: "fake text",
+  //   descendants: 0,
+  //   kids: []
+  // };
   @Input() item: Item;
-
-  constructor() {}
-
-  ngOnInit() {}
 }
