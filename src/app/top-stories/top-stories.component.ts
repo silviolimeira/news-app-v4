ce5882fd52a10583ab6a590811cff626fb8a29da
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-07 08:36:29 -0300

    Listing 5-25 top-stories.ts

--- src/app/top-stories/top-stories.component.ts
+++ src/app/top-stories/top-stories.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+
+import { Subscription } from "rxjs";
+import { Items } from "../models/items";
+import { ItemService } from "../services/item/item.service";
+import { ItemsComponent } from "../components/items/items.component";
 
 @Component({
-  selector: 'app-top-stories',
-  templateUrl: './top-stories.component.html',
-  styleUrls: ['./top-stories.component.scss'],
+  selector: "app-top-stories",
+  templateUrl: "./top-stories.component.html",
+  styleUrls: ["./top-stories.component.scss"]
 })
-export class TopStoriesComponent implements OnInit {
+export class TopStoriesComponent implements OnInit, OnDestroy {
+  items: Items;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private itemService: ItemService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscription = this.itemService
+      .load(0, 10)
+      .subscribe(items => (this.items = items));
+  }
 
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
