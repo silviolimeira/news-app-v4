ed5a9597779997974757616bc75202bba9cdba01
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-10 10:46:23 -0300

    add body-parser and configure post method

--- server/v1/server.js
+++ server/v1/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "jes");
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/show", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(3333, function() {
