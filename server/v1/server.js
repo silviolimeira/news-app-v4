fc874424ac181e84ca7eeac0f096f37675d7f237
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-10 11:17:55 -0300

    add mongodb and post end-point

--- server/v1/server.js
+++ server/v1/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const mongoClient = require("mongodb").MongoClient;
+
+const uri = "mongodb://localhost:27017/myDB";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,10 +14,20 @@ app.get("/", (req, res) => {
 });
 
 app.post("/show", (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
+  db.collection("data").insertOne(req.body, (err, result) => {
+    if (err) return res.sendStatus(400);
+
+    console.log("salvo no banco de dados");
+    res.redirect("/");
+  });
+  //   res.sendStatus(200);
 });
 
-app.listen(3333, function() {
-  console.log("Server running on port 3333...");
+mongoClient.connect(uri, (err, client) => {
+  if (err) return console.log("err");
+  db = client.db("myDB");
+
+  app.listen(3333, function() {
+    console.log("Server running on port 3333...");
+  });
 });
