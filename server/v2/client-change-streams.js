c345fda59d75939e7b6fb102d2454fc9df452f1b
Silvio Cesar Marqui Limeira <silvio.limeira@mstech.com.br>
2019-06-13 10:35:34 -0300

    add client-change-streams.js for monitoring Change Streams With MongoDB

---
+++ server/v2/client-change-streams.js
@@ -0,0 +1,18 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const pipeline = [{ $project: { documentKey: false } }];
+
+MongoClient.connect("mongodb://localhost:27017/?replicaSet=rs0").then(
+  client => {
+    console.log("Connected correctly to server");
+    // specify db and collections
+    const db = client.db("myDB");
+    const collection = db.collection("messages");
+    console.log(MongoClient);
+    const changeStream = collection.watch(pipeline);
+    // start listen to changes
+    changeStream.on("change", function(change) {
+      console.log(JSON.stringify(change));
+    });
+  }
+);
